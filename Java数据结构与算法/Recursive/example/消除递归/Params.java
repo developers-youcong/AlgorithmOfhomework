efde74552a45185ca4d152c2cbf3ba12a1dfa521
developers-youcong <15200740587@163.com>
2019-08-21 23:11:32 +0800

    feat:增加递归例子和对应的完整笔记

---
+++ Java数据结构与算法/Recursive/example/消除递归/Params.java
@@ -0,0 +1,15 @@
+package cn.algorithm.recursive.simple;
+
+public class Params {
+
+	public int n;
+	public int returnAddress;
+	public Params(int nn,int ra) {
+		
+		n = nn;
+		returnAddress = ra;
+	}
+	
+	
+
+}
---
+++ Java数据结构与算法/Recursive/example/消除递归/StackX.java
@@ -0,0 +1,29 @@
+package cn.algorithm.recursive.simple;
+
+public class StackX {
+
+    private int maxSize;
+    private Params[] stackArray;
+    private int top;
+	
+	public StackX(int s) {
+		maxSize = s;
+		stackArray = new Params[maxSize];
+		top = -1;
+	}
+	
+	public void push(Params p) {
+		stackArray[++top] = p;
+	}
+	
+	public Params pop() {
+		
+		return stackArray[top--];
+	}
+
+	public Params peek() {
+		return stackArray[top];
+	}
+	
+	
+}
